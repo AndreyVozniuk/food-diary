9029fb127628786cf48c4cb8df954b4a556cd823
Andrew <vozniukandrey@gmail.com>
2020-11-24 20:08:04 +0200

    Updates

---
+++ precache-manifest.f09905a28c333f9f10588e2781564f9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f315b631943033832b3b490b945d6f",
+    "url": "/food-diary/index.html"
+  },
+  {
+    "revision": "ef3a50dcba59cc43e10f",
+    "url": "/food-diary/static/css/main.088384a4.chunk.css"
+  },
+  {
+    "revision": "0d5f81aeb0ca339081c2",
+    "url": "/food-diary/static/js/2.1ca0fff0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-diary/static/js/2.1ca0fff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3a50dcba59cc43e10f",
+    "url": "/food-diary/static/js/main.7a5f5f00.chunk.js"
+  },
+  {
+    "revision": "4206dde888c3a16bc227",
+    "url": "/food-diary/static/js/runtime-main.ea0ab2e8.js"
+  }
+]);
